ecf8199d4c32510f163999b8e58a3da0411d8871
Seung Chi <seungyoonchi@gmail.com>
2017-09-18 11:43:46 -0700

    set up for web, models, services, db

---
+++ Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Academy.Web.MvcApplication" Language="C#" %>
---
+++ Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Academy.Web.Startup))]
+namespace Academy.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
