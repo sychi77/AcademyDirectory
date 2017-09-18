8619f7a76086246cb1d1a17448a764b00b5f2163
Seung Chi <seungyoonchi@gmail.com>
2017-09-18 15:25:30 -0700

    added unit test starter

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Academy.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Academy.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80a8e0f3-371f-4a7c-840a-d7c636cf7292")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Services/StudentsServicesTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Academy.Services;
+using Academy.Models.Domain;
+using System.Collections.Generic;
+
+namespace Academy.Tests
+{
+    [TestClass]
+    public class StudentsServicesTests
+    {
+        [TestMethod]
+        public void StudentsServices_SelectAll_Test()
+        {
+            StudentsService svc = new StudentsService();
+            List<Students> model = svc.SelectAll();
+            Assert.IsNotNull(model);
+        }
+    }
+}
